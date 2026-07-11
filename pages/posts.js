08995dde3e2cd80b921d92e605bb09c75656ce8b
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 13:46:57 -0400

    add routing functionality

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,10 @@
+import Navbar from "../Components/Navbar"
+
 export default function Posts(){
     return (
+        <>
+        <Navbar />
         <h1>This is the Posts Page</h1>
-    )
-}
+        </>
+        )
+    }
--- pages/users.js
+++ pages/users.js
@@ -1,5 +1,10 @@
+import Navbar from "../Components/Navbar"
+
 export default function Users(){
     return (
+        <>
+        <Navbar />  
         <h1>This is the Users Page</h1>
-    )
-}
+        </>
+        )
+    }
