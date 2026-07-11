026001069ac1aa4fe857e56aff05b2c4acfa260c
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 17:31:19 -0400

    added server side

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
 
-export default function Posts() {
+export async function getServerSideProps() {
+   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+   const data = await res.json();
+
+    return {
+      props: {
+        posts: data
+      }
+    }
+   
+}
+
+export default function Posts({posts}) {
 
   
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-      setPosts(data);
-
-    }
-      fetchPosts();
-
-  }, []);
+  //const [posts, setPosts] = useState([]);
+ // useEffect(() => {
+ //   const fetchPosts = async () => {
+ //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+ //     const data = await res.json();
+ //     setPosts(data);
+//
+//    }
+//      fetchPosts();
+//
+//  }, []);
 
   return (
     <>
       <h1>This is the Posts Page</h1>  
-      {posts.map(post => (
+      {posts?.map(post => (
         <li key={post.id}>{post.title}</li>
       ))}
     </> 
