21cf8e64ce45b2bb1bce9b5082feac5e98ab5399
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 17:12:01 -0400

    SSR Updated code

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Posts() {
+
+  
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setPosts(data);
+
+    }
+      fetchData();
+
+  }, []);
+
   return (
     <>
-      <h1>This is the Posts Page</h1>
-    </>
+      <h1>This is the Posts Page</h1>  
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </> 
   )
-}   
+}
