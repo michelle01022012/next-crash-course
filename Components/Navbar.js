f97cc2713b1ead7f70089f30d08c85ede4e25e90
Michelle Anderson <manderson0102@yahoo.com>
2026-07-10 21:31:54 -0400

    adding styles

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,3 +1,6 @@
+import styles from "../styles/Navbar.module.css"
+
+
 export default function Navbar() {
   return (
     <nav>
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,12 @@ export default function Home() {
   return (
     <>
      <Navbar />
+     <ul>
+       <li>Item 1</li>
+       <li>Item 2</li>
+       <li>Item 3</li>
+       <li>Item 4</li>
+     </ul>
     </>
   )
 }
