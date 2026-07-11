d9b156a7a7e4dd21f70a6b342bfd7923f320353c
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 14:48:03 -0400

    added dynamic route

--- pages/users/jack.js
+++
@@ -1,7 +0,0 @@
-export default function Jack() {
-  return (
-    <>
-      <h1>This is the user: Jack</h1>
-    </>
-  )
-}
--- pages/users/jack/post1.js
+++
@@ -1,7 +0,0 @@
-export default function Jack() {
-  return (
-    <>
-      <h1>This is Jack Post 1</h1>
-    </>
-  )
-}   
