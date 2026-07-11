78bcfa09c7dcc71674656fa6260e73941f70ca67
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 11:56:28 -0400

    updated styles

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,8 +3,8 @@ import styles from "../styles/Navbar.module.css"
 
 export default function Navbar() {
   return (
-    <nav>
-        <ul>
+   <nav className={styles.nav}>
+        <ul className={styles.ul}>
             <li>Home</li>
             <li>Posts</li>
             <li>Users</li>
