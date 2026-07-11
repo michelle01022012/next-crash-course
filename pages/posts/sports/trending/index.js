a714ce631864ede33a720b17659c5779057cf26c
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 14:31:41 -0400

    added trending sports for Jack

---
+++ pages/posts/sports/trending/index.js
@@ -0,0 +1,7 @@
+export default function Posts() {
+  return (
+    <>
+      <h1>This is the Trending Page</h1>
+    </>
+  )
+}   
---
+++ pages/posts/sports/trending/jack.js
@@ -0,0 +1,7 @@
+export default function Posts() {
+  return (
+    <>
+      <h1>These are the sports related trending posts by Jack</h1>
+    </>
+  )
+}
