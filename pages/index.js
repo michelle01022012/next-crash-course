c75caa854e7ca822d8579641fbd09b43a566a880
Michelle Anderson <manderson0102@yahoo.com>
2026-07-11 13:09:58 -0400

    updated colors

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-//import Navbar from '@/Components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/Components/Navbar'
 
 export default function Home() {
   return (
     <>
- <ul>
-  <li>Item 1</li>
-  <li>Item 2</li>
-  <li>Item 3</li>
-  <li>Item 4</li>
-</ul>
+      <Navbar />
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
     </>
   )
 }
